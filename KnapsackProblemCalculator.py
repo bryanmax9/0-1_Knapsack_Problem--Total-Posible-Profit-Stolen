da532bcb83e63ff819af47d3f326c4c9fc9a6164
Bryan Tineo <brillant16.gcv@gmail.com>
2023-10-02 13:22:49 -0700

    Fixed reverse trversal

--- KnapsackProblemCalculator.py
+++ KnapsackProblemCalculator.py
@@ -16,7 +16,7 @@ def knapsack_01(capacity, items):
     for i in range(1, n + 1):
       for w in range(capacity + 1):
         # Unpacking the weight and value for current item
-        weight, value, _ = items[i - 1] # example: v1=2, w1=3
+        value, weight, _ = items[i - 1] # example: v1=1, w1=2
             
         if weight <= w:
           dp[i][w] = max(dp[i-1][w], dp[i-1][w-weight] + value) # we are going to either place the one above the current value, or include the previus item that is positioned in index "[w-weight]" plus the current value
@@ -27,29 +27,38 @@ def knapsack_01(capacity, items):
     selected_items = [] #here we will store the items that get the most pofit based on the table we created above
     w = capacity
     
-    #we will go in reverse from the bottom right corner to find the maximum profit and the items to be considered
+    # We start the backtracking from the last item (n) and move towards the first (1).
     for i in range(n, 0, -1):
+      
+      # Extracting the value, weight, and quantity of the current item.
+      value, weight, _ = items[i-1]
+      
+      # If the value at the current cell (dp[i][w]) is different from the value right above it (dp[i-1][w]),
+      # it indicates that the current item (items[i-1]) was included in the optimal solution for the current 
+      # weight w. This is the essence of the dynamic programming solution for the 0/1 knapsack problem.
       if dp[i][w] != dp[i-1][w]:
-        # if the item abovce is different then we will go to the index that is calculated by substracting the weight of the column minus weigth of the item "[w-weight]" and use the index to go to the above item
-        #we will first append the current row since it was selected   
+        
+        # As this item is included in the optimal solution, we add its index to the selected_items list.
         selected_items.append(i)
         
-        #each item in the list is a tuple of the form (weight, value, quantity)
-        #therefore we will update the weight in order to go to the next value above us 
-        w -= items[i-1][0]
+        # We then subtract the weight of this item from w to move left in the DP table. This represents 
+        # the remaining weight after including the current item in the solution.
+        w -= weight
 
     # Returning the max value ,the dp table, and selected items 
     return dp[n][capacity], dp, selected_items
 
 # Reading input from file
-filename = "example.txt"
+filename = "10.txt"
 capacity, items = parse_input(filename)
 
 max_profit, table, selected = knapsack_01(capacity, items)
+actual_selected_items = [items[i-1] for i in selected]
+print("Actual selected items:", actual_selected_items)
 
 print("Maximum profit:", max_profit)
 print("Selected items:", selected)
 
 # Printing the DP table (uncomment the code below if you are using smalldatasets)
-for row in table:
-    print(row)
+# for row in table:
+#     print(row)
